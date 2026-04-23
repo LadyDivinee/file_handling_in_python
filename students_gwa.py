c854e57296326a05030fb63ca7cc2969a0b759f9
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 15:44:19 -0700

    load students data

--- students_gwa.py
+++ students_gwa.py
@@ -3,3 +3,8 @@ class StudentGwaAnalyzer:
     def __init__(self, student_file):
         self.student_file = student_file
         self.student_records = []
+#load students data
+    def student_data(self):
+        file_connection = open(self.student_file, "r")
+        self.student_records = file_connection.readlines()
+        file_connection.close()
