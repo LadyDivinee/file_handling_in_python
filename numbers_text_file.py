5fed65598d74555cf55ad841d27439d963b4e7cd
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 00:14:33 -0700

    open and read step

--- numbers_text_file.py
+++ numbers_text_file.py
@@ -3,4 +3,7 @@ class NumberSeparator:
         self.source_filename = source_filename
         self.extracted_lines = []
 
-    
+    def load_numbers_from_file(self):
+        read_connection = open(self.source_filename, "r")
+        self.extracted_lines = read_connection.readlines()
+        read_connection.close()
