6ea2cb8327d3af7f5a9dbf063bd8abe9b24e4fca
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 15:58:48 -0700

    find highest GWA

--- students_gwa.py
+++ students_gwa.py
@@ -8,3 +8,16 @@ class StudentGwaAnalyzer:
         file_connection = open(self.student_file, "r")
         self.student_records = file_connection.readlines()
         file_connection.close()
+#find highest gwa
+    def print_highest_gwa(self):
+        top_name = ""
+        lowest_number = 5.0
+
+        for line in self.student_records:
+            name, gwa = line.strip().split(",")
+            gwa = float(gwa)
+
+            if gwa < lowest_number:
+                lowest_number = gwa
+                top_name = name
+                
