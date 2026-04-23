d70d474d91de1e7019fa50bb3d9bf8b8911fd80c
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 00:45:20 -0700

    fixed errors and final code

--- numbers_text_file.py
+++ numbers_text_file.py
@@ -12,15 +12,17 @@ class NumberSeparator:
     def partition_and_save_numbers(self, user_select):
         if user_select == 0:
             output_filename = "even.txt"
+            remainder = 0
         else:
             output_filename = "odd.txt"
+            remainder = 1
 
         write_connection = open(output_filename, "w")
 
         for i in range (0, len(self.extracted_lines)):
             integer_value = int(self.extracted_lines[i].strip())
 
-            if integer_value % 2 == user_select:
+            if integer_value % 2 == remainder:
                 write_connection.write(str(integer_value) + "\n")
 
         write_connection.close()
@@ -29,5 +31,9 @@ def main():
     separator = NumberSeparator("numbers.txt")
     separator.load_numbers_from_file()
 
-    user_input = int(input("Enter 2 for even numbers, 1 for odd numbers to save: "))
+    user_input = int(input("Enter 0 for even numbers, 1 for odd numbers to save: "))
     separator.partition_and_save_numbers(user_input)
+
+    print("File created successfully!")
+
+main()
