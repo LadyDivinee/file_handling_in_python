6a478eebce021ef8de1315ce0add183eb9a22d12
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 21:38:02 -0700

    #asking the input from the user

--- multiple_line_into_text_file.py
+++ multiple_line_into_text_file.py
@@ -2,3 +2,6 @@
 class MottosInLife:
     def __init__(self):
         self.filename = "mylife.txt"
+#asking the input from the user
+    def enter_motto(self):
+        file = open(self.filename, "a")
