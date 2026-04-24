a5f8b9fa691a01c7a58ee5082d031d73e83c24f1
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-23 21:51:33 -0700

    for loop asking

--- multiple_line_into_text_file.py
+++ multiple_line_into_text_file.py
@@ -5,3 +5,10 @@ class MottosInLife:
 #asking the input from the user
     def enter_motto(self):
         file = open(self.filename, "a")
+#for loop asking
+        while True:
+            input_line = input("Hi! Enter your motto in life: ")
+            print(input_line, file = file)
+            options_from_user = input("Do you want to add more? Yes or No: ").lower()
+            if options_from_user == "no":
+                break
