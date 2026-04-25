0a3d404412ccd74e0d8efad7f706a0c8568984a7
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-24 22:48:52 -0700

    instance method, read and write for the output

--- double_triple_integers.py
+++ double_triple_integers.py
@@ -2,3 +2,8 @@ class IntegerNumbers:
 #class constructor
     def __init__(self):
         self.file = "integers.txt"
+#instance method, read and write for the output
+    def integers_in_file(self):
+        file_of_integers = open(self.file, "r")
+        even_file = open("double.txt", "w")
+        odd_file = open("triple.txt", "w")
