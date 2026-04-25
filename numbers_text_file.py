784490769b9dd9255f4840370607342a6d0f9a76
LadyDivinee <ladydivineortega14@gmail.com>
2026-04-25 09:05:43 -0700

    fixed to coding standard

--- numbers_text_file.py
+++ numbers_text_file.py
@@ -1,11 +1,11 @@
 #class constructor
 class NumberSeparator:
-    def __init__(self, source_filename):
-        self.source_filename = source_filename
+    def __init__(self, source_file_name):
+        self.source_file_name = source_file_name
         self.extracted_lines = []
 #open and read step
     def load_numbers_from_file(self):
-        read_connection = open(self.source_filename, "r")
+        read_connection = open(self.source_file_name, "r")
         self.extracted_lines = read_connection.readlines()
         read_connection.close()
 #partition and save numbers using loop and conditional statement
@@ -16,15 +16,12 @@ class NumberSeparator:
         else:
             output_filename = "odd.txt"
             remainder = 1
-
         write_connection = open(output_filename, "w")
 
         for i in range (0, len(self.extracted_lines)):
             integer_value = int(self.extracted_lines[i].strip())
-
             if integer_value % 2 == remainder:
                 write_connection.write(str(integer_value) + "\n")
-
         write_connection.close()
 #main execution for user to input desired numbers
 def main():
